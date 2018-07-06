9772614743d169b280a4323b91459f2257098e3e
MG <mg84@bk.ru>
2018-07-06 14:31:04 +0300

    protractor config cmpl

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,22 +1,16 @@
-//jshint strict: false
-exports.config = {
-
+module.exports.config = {
   allScriptsTimeout: 11000,
-
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
-    '*.js'
+    './test.spec.js'
   ],
-
   capabilities: {
     'browserName': 'chrome'
   },
-
-  baseUrl: 'http://localhost:8000/',
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+  baseUrl: 'http://localhost:3000/',
+  framework: 'mocha',
+  mochaOpts: {
+    reporter: 'spec',
+    timeout: 10000
   }
-
 };
